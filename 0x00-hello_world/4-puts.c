f4e39b74ef853d4b34e9c3458504ba9ec032c123
Tunmise <falokuntunmise@gmail.com>
2023-06-15 16:15:01 -0700

    task2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Printing
-*
-* Return:0 (Successful)
-*/
+ * main - Printing
+ *
+ * Return:0 (Successful)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 return (0); 
 }
